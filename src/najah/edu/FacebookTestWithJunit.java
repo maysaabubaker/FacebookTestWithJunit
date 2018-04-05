9f013ef6a1b1c36766d47b42fad7d7f066c2c7aa
Maysa Abu Baker <Maysa Abu Baker@MaysaAb>
2018-04-05 22:16:33 +0300

    first commit

---
+++ src/najah/edu/FacebookTestWithJunit.java
@@ -0,0 +1,38 @@
+package najah.edu;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FacebookTestWithJunit {
+	static WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+		
+		driver=new FirefoxDriver();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("maysaabubaker@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("05923115162081997maza");
+		driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
+	}
+
+}
